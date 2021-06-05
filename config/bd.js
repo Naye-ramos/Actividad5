14a02e4f7235d5d163b08feab1d5dccf06fd247d
naye_ramos <nayeocampo2108@gmail.com>
2021-06-05 11:10:20 -0500

    CRUD de Cliente

--- config/bd.js
+++ config/bd.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 // exportar una instancia de Sequelize con la conexión a nuetsra BD.
 // agregar la información para conectar a la bd
-module.exports = new Sequelize('', '', '', {
+module.exports = new Sequelize('backend_api', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
     });
